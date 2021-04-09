f52f4bb3f5e52662f2901aa59eeeb6ad2aa5e8d0
pomkos <albeithome@gmail.com>
2021-04-09 18:36:37 +0000

    move pypi stuff to new folder for readability

--- pypi/setup.py
+++ pypi/setup.py
@@ -1,7 +1,7 @@
 # create wheel/dist with `python setup.py sdist bdist_wheel`
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("../README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
