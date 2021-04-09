e913124dc3a06e7e6ebef20a1903dce154899bc5
chand1012 <chandler@chand1012.dev>
2021-04-09 13:10:42 -0400

    Change to install_requires, manually added packages

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     version= "1.0.0.3",
     author="Peter Gates",
     author_email="pgate89@gmail.com",
-    description='Algorithms for Pillar. Curently includes "mini" algorithms, nothing too sophisticated.',
+    description='Algorithms for Pillar. Currently includes "mini" algorithms, nothing too sophisticated.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pomkos/twitch_chat_analysis",
@@ -24,6 +24,11 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Information Analysis"
     ],
     package_dir={"": "pillaralgos"},
-    packages=setuptools.find_packages(where="pillaralgos"),
+    install_requires=[
+        'pandas>=1.2.3',
+        'numpy>=1.20.2',
+        'matplotlib>=3.4.1',
+        'seaborn>=0.11.1'
+    ],
     python_requires=">=3.6",
 )
