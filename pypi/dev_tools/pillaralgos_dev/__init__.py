578866af07d49da3c3d2b64c060a76945ede3a64
pomkos <albeithome@gmail.com>
2021-04-11 18:45:50 +0000

    reorganized, ready for poetry build

---
+++ pypi/dev_tools/pillaralgos_dev/__init__.py
@@ -0,0 +1,10 @@
+'''
+Functions and classes to help with development. Stuff that isn't required in 
+prod for algos to work. Like downloading files from AWS, renaming files, 
+graphing, sanity checks, and so on.
+
+HOW TO:
+    from pillaralgos_dev import dev_helpers as dev
+    from pillaralgos_dev import sanity_checks as sane
+'''
+__version__ = '0.1.0'
