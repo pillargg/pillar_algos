986b4b0c0eeb918c7a45c70d2eb5d181d9586606
pomkos <albeithome@gmail.com>
2021-04-15 13:45:33 -0400

    Update brain.py

--- prod/pillaralgos/brain.py
+++ prod/pillaralgos/brain.py
@@ -6,7 +6,7 @@ from pillaralgos import algo1, algo2, algo3_0, algo3_5
 import json
 import numpy as np
 
-def run(data, x, limit=None):
+def run(data, common_timestamps, algos_to_compare = ["algo1","algo2","algo3_0","algo3_5"], limit=None):
     '''
     Coordinates all 4 algorithms, then compares across results to see if any timestamps are shared between them. Runs all algos on default param settings.
     
@@ -14,8 +14,10 @@ def run(data, x, limit=None):
     -----
     data: list
         List of dictionaries, like a json file in the format [{'startTime':float, 'endTime':float}]
-    x: int
+    common_timestamps: int
         Cutoff for how many algos should have a timestamp for it to be included in the results
+    algos_to_compare: list
+        List of one of: "algo1","algo2","algo3_0","algo3_5"
     limit: int or None
         How many results should be returned. If None, all results surviving the X filter will be returned.
     
@@ -23,10 +25,22 @@ def run(data, x, limit=None):
     ------
     new_json: list
         List of dictionaries, [{'startTime':float, 'endTime':float}]
+        Or 
     '''
-    # gather results from algos
+    if len(algos_to_compare) < 1:
+        return "algos_to_compare cannot be empty"
+    # comply with user input
+    compare_us = []
+    if "algo1" in algos_to_compare:
+        compare_us.append(algo1)
+    if "algo2" in algos_to_compare:
+        compare_us.append(algo2)
+    if "algo3_0" in algos_to_compare:
+        compare_us.append(algo3_0)
+    if "algo3_5" in algos_to compare:
     results = []
-    for algo in [algo1, algo2, algo3_0, algo3_5]:
+    # gather results from algos
+    for algo in algos_to_compare:
         result = algo.run(data, min_=2)
         results.append(result)
 
@@ -38,7 +52,7 @@ def run(data, x, limit=None):
         results_df = results_df.append(result)
 
     # get valuecounts as a list, filter out timestamps that only occur once
-    common = list(results_df['startTime'].value_counts()[results_df['startTime'].value_counts(sort=True) > x].index)
+    common = list(results_df['startTime'].value_counts()[results_df['startTime'].value_counts(sort=True) > common_timestamps].index)
 
     common_results = pd.DataFrame(columns=['startTime','endTime'])
 
@@ -46,16 +60,16 @@ def run(data, x, limit=None):
         if row['startTime'] in common:
             common_results = common_results.append(row)
 
+    if len(common_results) < 1:
+        return np.array()
+    
     common_results = common_results.drop_duplicates()
-
+    
     new_json = []
     for i, row in common_results.iterrows():
         new_json.append({'startTime':row['startTime'], 'endTime':row['endTime']})
         
-    if not new_json:
-        return np.array()
-
-    if limit == None:
-        return new_json
-    else:
+    if limit:
         return new_json[:limit]
+    else:
+        return new_json
