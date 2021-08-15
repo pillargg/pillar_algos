ea5a82f04dec5081a0cc6643a8ca639cc52441b8
pomkos <albeithome@gmail.com>
2021-08-15 02:38:47 +0000

    updated brain to create feature sets

--- prod/pillaralgos/brain.py
+++ prod/pillaralgos/brain.py
@@ -1,39 +1,66 @@
-"""
-Coordinates all 4 algorithms, then compares across results to see if any timestamps are shared between them
-"""
 import pandas as pd
 import numpy as np
+import json
 
-from pillaralgos import algo1, algo2, algo3_0, algo3_5, algo3_6, algo4
-from .helpers import data_handler as dh
+from pillaralgos import algo1, algo2, algo3_0, algo3_5, algo3_6, algo4, brain
+from pillaralgos.helpers import data_handler as d
+from pillaralgos.helpers import exceptions as e
+
+from icecream import ic
 
 ### Functions in common across all algos ###
 def load_data(data, clip_length):
     """
     Loads and splits data into appropriate chunks
+
+    output
+    ------
+        first_stamp: timestamp of the first chat message
+        chunk_df: dataframe of chunks, retaining all info from big_df
+        vid_id: stream id of chat transcript
     """
+    from pillaralgos.helpers import data_handler as dh
+
     big_df = dh.organize_twitch_chat(data)  # organize
     if type(big_df) == pd.DataFrame:
         first_stamp, chunks_list = dh.get_chunks(big_df, min_=clip_length)
         vid_id = data[0]["content_id"]
-        return (big_df, first_stamp, chunks_list, vid_id)
+        chunk_df = pd.DataFrame()
+
+        for dataframe in chunks_list:
+            dataframe = dataframe.sort_values("created_at")
+            dataframe["start"] = dataframe.iloc[0, 0]
+            dataframe["end"] = dataframe.iloc[-1, 0]
+            chunk_df = chunk_df.append(dataframe)
+
+        chunk_df = chunk_df.reset_index(drop=True)
+        return (first_stamp, chunk_df, vid_id, chunks_list)
     else:
         return np.array([])
 
 
 def format_json_results(results, first_stamp, sort_by):
+    """
+    At the very end, jsonify all results
+    """
+    from pillaralgos.helpers import data_handler as dh
+
     json_results = dh.results_jsonified(results, first_stamp, sort_by)
 
     return json_results
 
 
-### Helper functions
-def format_feature_df():
+def format_feature_df(dataframe: pd.DataFrame, select: tuple) -> pd.DataFrame:
     """
     Helper function to create the feature set from which ML algorithm will be trained
+
+    input
+    -----
+    select: tuple containing, in order, desired algo1, algo2, algo3,
+            algo3_5, algo3_6, algo4 features
     """
-    feature_df = pd.DataFrame(
-        columns=[
+    feature_df = dataframe[
+        [
             "vid_id",
             "startTime",
             "endTime",
@@ -44,100 +71,55 @@ def format_feature_df():
             "algo4_metric",
             "ccc_label",
         ]
-    )
+    ]
+    return feature_df
 
 
-### Run selected algos ###
-def run(
-    data,
-    clip_length,
-    common_timestamps=2,
-    algos_to_compare=["algo1", "algo2", "algo3_0", "algo3_5"],
-    limit=None,
-    save_json=False,
-):
+def test_results(data, dataframe):
     """
-    Coordinates all 4 algorithms, then compares across results to see if any timestamps are shared between them. Runs all algos on default param settings.
-
-    input
-    -----
-    data: list
-        List of dictionaries, like a json file in the format [{'startTime':float, 'endTime':float}]
-    common_timestamps: int
-        Cutoff for how many algos should have a timestamp for it to be included in the results
-    algos_to_compare: list
-        List of one of: "algo1","algo2","algo3_0","algo3_5"
-    limit: int or None
-        How many results should be returned. If None, all results surviving the X filter will be returned.
-
-    output
-    ------
-    new_json: list
-        List of dictionaries, [{'startTime':float, 'endTime':float}]
-        Or
+    Checks that we retained all chunks, and that chunks have start/end times
     """
-    # load and format data the way each algo expects it
-    important_data = load_data(data, clip_length)
-    if type(important_data) != list:
-        # empty array signifies bad dataset. Russell requested to return empty array in that case
-        return {"error": "dataset was empty"}
+    from icecream import ic
 
-    if len(algos_to_compare) < 1:
-        return "algos_to_compare cannot be empty"
-    # comply with user input
-    compare_us = []
-    if "algo1" in algos_to_compare:
-        compare_us.append(algo1)
-    if "algo2" in algos_to_compare:
-        compare_us.append(algo2)
-    if "algo3_0" in algos_to_compare:
-        compare_us.append(algo3_0)
-    if "algo3_5" in algos_to_compare:
-        compare_us.append(algo3_5)
-    if "algo3_6" in algos_to_compare:
-        compare_us.append(algo3_6)
-    if "algo4" in algos_to_compare:
-        compare_us.append(algo4)
-    results = []
-    # gather results from algos
-    for algo in compare_us:
-        result = algo.run(data, min_=clip_length)
-        results.append(result)
+    num_chunks = len(data[-1])
+    num_results = len(dataframe)
 
-    # turn results into df
-    results_df = pd.DataFrame(columns=["startTime", "endTime"])
-    for i in range(len(results)):
-        result = results[i]
-        result = pd.DataFrame(result)
-        results_df = results_df.append(result)
-
-    # get valuecounts as a list, filter out timestamps that only occur once
-    common = list(
-        results_df["startTime"]
-        .value_counts()[
-            results_df["startTime"].value_counts(sort=True) >= common_timestamps
-        ]
-        .index
-    )
-
-    common_results = pd.DataFrame(columns=["startTime", "endTime"])
-
-    for i, row in results_df.iterrows():
-        if row["startTime"] in common:
-            common_results = common_results.append(row)
-
-    if len(common_results) < 1:
-        return np.array()
-
-    common_results = common_results.drop_duplicates()
-
-    new_json = []
-    for i, row in common_results.iterrows():
-        new_json.append({"startTime": row["startTime"], "endTime": row["endTime"]})
-
-    if save_json:
-        dh.save_json(new_json, f"brain")
-    if limit:
-        return new_json[:limit]
+    if (
+        (num_chunks == num_results)
+        & ("start" in dataframe.columns)
+        & ("end" in dataframe.columns)
+    ):
+        return True
     else:
-        return new_json
+        ic(num_results, num_chunks)
+        ic(dataframe.columns)
+        return False
+
+
+def run(vid_id: str, clip_length: int, limit: int = None, select: str = "all"):
+    # common settings
+
+    data = json.load(open(f"pillaralgos/data/{vid_id}.json"))
+    if len(data) == 0:
+        ic(f"{vid_id} is empty")
+        return f"{vid_id} is empty"
+    important_data = load_data(data, clip_length)
+
+    algo_results = {}
+    all_algos = [algo1, algo2, algo3_0, algo3_5, algo3_6, algo4]
+    ic("Running algorithms")
+    for i in range(len(all_algos)):
+        a = all_algos[i].featureFinder(important_data)
+        res = a.run()
+        formatted = d.data_finalizer(res, vid_id=vid_id)
+        if test_results(important_data, formatted):
+            algo_results[f"algo_{i}"] = formatted
+        else:
+            ic(f"algo_{i} failed the test!")
+            algo_results[f"algo_{i}"] = formatted
+
+    df = algo_results["algo_0"]
+    ic("Merging featuresets")
+    for key in algo_results.keys():
+        if key != ["algo_0"]:
+            df = df.merge(algo_results[key])
